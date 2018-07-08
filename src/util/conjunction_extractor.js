21bfefd9aac7a9c7f85dd24d005540b7c163ae4d
nixer89 <pbnixer@gmail.com>
2018-07-08 21:09:57 +0200

    improvements in parsing the text and tests

--- src/util/conjunction_extractor.js
+++ src/util/conjunction_extractor.js
@@ -1,5 +1,5 @@
 var jsonConstruct = {
-    "Präsens" : {
+    "Praesens" : {
         "Indikativ": {
             "Singular": {
                 "ich": "",
@@ -12,7 +12,7 @@ var jsonConstruct = {
                 "sie": ""
             }
         },
-        "Konjunktiv 1": {
+        "Konjunktiv1": {
             "Singular": {
                 "ich": "",
                 "du": "",
@@ -50,7 +50,7 @@ var jsonConstruct = {
                 "sie": ""
             }
         },
-        "Konjunktiv 2": {
+        "Konjunktiv2": {
             "Singular": {
                 "ich": "",
                 "du": "",
@@ -63,8 +63,8 @@ var jsonConstruct = {
             }
         }
     },
-    "Partizip 1": "",
-    "Partizip 2": "",
+    "Partizip1": "",
+    "Partizip2": "",
     "Infinitiv mit": ""
 }
 
@@ -76,11 +76,29 @@ module.exports = class ConjunctionExtractor {
             "forms": jsonConstruct
         }
 
-        var textToParse1 = text.substring(text.indexOf("ich"), text.indexOf("Präteritum"));
-        console.log(textToParse1);
+        var textToParsePraesens = text.substring(text.indexOf("ich"), text.indexOf("Plural"));
+        
+        
+        console.log(text + "\n\n");
+        
+        this.textSplitter(structure, textToParsePraesens, "ich", "du");
+        this.textSplitter(structure, textToParsePraesens, "du", "er/sie/es");
+        this.textSplitter(structure, textToParsePraesens, "er/sie/es", "wir");0
+
+        var textToParsePraesens = text.substring(text.indexOf("Plural"), text.indexOf("Präteritum"));
+        this.textSplitter(structure, textToParsePraesens, "wir", "ihr");
+        this.textSplitter(structure, textToParsePraesens, "ihr", "sie");
+        
+        console.log(JSON.stringify(structure));
     }
 
-    textSplitter(startText, endText) {
+    textSplitter(structure, textToParse, formToFind, nextForm) {
 
+        var textToParseForm = textToParse.substring(textToParse.indexOf(formToFind),textToParse.indexOf(nextForm == undefined ? text.textToParse.length : nextForm)).split(formToFind);
+        
+        console.log(textToParseForm)
+        structure.forms.Praesens.Indikativ.Singular[formToFind] = textToParseForm[1] != undefined ?  textToParseForm[1].trim() : undefined;
+        structure.forms.Praesens.Konjunktiv1.Singular[formToFind] = textToParseForm[2] != undefined ?  textToParseForm[2].trim() : undefined;
+        structure.forms.Praesens.Imperativ.Singular[formToFind] = textToParseForm[3] != undefined ?  textToParseForm[3].trim() : undefined;
     }
 }
